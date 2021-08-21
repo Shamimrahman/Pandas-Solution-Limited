ef2227b40f0125793bd0a269f768ad0348844c39
unknown <shamim.rahman@northsouth.edu>
2021-08-21 23:37:23 +0600

    updated

--- client/src/components/estimate/Estimate.jsx
+++ client/src/components/estimate/Estimate.jsx
@@ -17,6 +17,7 @@ const Estimate = () => {
         <li>B</li>
         <li>B</li>
         <li>B</li>
+        <li>B</li>
 
         <input placeholder="name" type="text"></input>
         <input placeholder="name" type="text"></input>
